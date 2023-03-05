817256f87c4e5ae23b53e3766477bd1d7f9862e0
SergeyNVKZ1991 <sergeynekrasov29121991@gmail.com>
2023-03-05 20:51:55 +0700

    revision 3

--- Python_regular.py
+++ Python_regular.py
@@ -1,37 +1,68 @@
 from pprint import pprint
-## Читаем адресную книгу в формате CSV в список contacts_list:
 import csv
 import re
-# data_file = []
+
+# читаем адресную книгу в формате CSV в список contacts_list
 with open("Data_file.csv", encoding='utf-8') as f:
-    reader = csv.reader(f)
-    date_phone = ''
-    for a in f:
-        date_phone += a
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
-pattern = r"(\+7|7|8)?[ |s]?([\(]?(\d{3})[\)]?)[ |s|-]*(\d+)[ |s|-]*(\d{2})[s|-]*(\d{2})"
-result = re.sub(pattern, r"+7 (\3)\4-\5-\6", date_phone)
+# Создаем новый список контактов, где ФИО помещены в отдельные столбцы lastname, firstname, surname
 
+new_list = []
+for contact in contacts_list:
+    contact[:3] = [' '.join(contact[:3])]
+    new_list.append(contact)
 
-text = result.split('\n')
-# pprint(text)
-splitted = [[]]
-for i in text[1:]:
-    splitted.append([i])
+new_contact_list = []
+for el in new_list:
+    for record in el:
+        record = el[0].split()
+        record.extend(el[1:])
+    new_contact_list.append(record)
 
-# dict_persone = {}
-# for string in splitted:
-#     for i in string:
-#         if string[0] not in dict_persone:
-#             dict_persone[string[0]] = string[1:]
-# #
-# pprint(dict_persone)
+# Приводим все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
 
+pattern = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)' \
+          '(\s*)(\-*)(\d{3})(\-)*(\d{2})(\-)*(\d{2})*' \
+          '(\s)*(\()*(доб\.)*(\s)*(\d+)*(\))*'
 
-## 2. Сохраните получившиеся данные в другой файл.
-## Код для записи файла в формате CSV:
-# with open("phonebook.csv", "w", encoding='utf-8') as f:
-#        datawriter = csv.writer(f, delimiter=',')
-#
-#        # Вместо contacts_list подставьте свой список:
-#        datawriter.writerows(dict_persone)
+repl = r'+7(\4)\8-\10-\12 \15\17'
+
+new_list = []
+for new_contact in new_contact_list:
+    contacts_string = ','.join(new_contact)
+    new_contacts_string = re.sub(pattern, repl, contacts_string)
+    format_contacts = new_contacts_string.split(',')
+    new_list.append(format_contacts)
+# pprint(new_list)
+
+# # Объединяем все дублирующиеся записи о человеке в одну.
+new_list_2 = []
+new_dict = {}
+res = {}
+
+for i in new_list:
+    # if record in new_list_2:
+    for record in new_list_2:
+        if i[0] in record.keys():
+
+            for v in record.values():
+                if v[0] == '':
+                    v[0] = i[1]
+                elif v[1] == '':
+                    v[1] = i[2]
+                elif v[2] == '':
+                    v[2] = i[3]
+                elif v[3] == '':
+                    v[3] = i[4]
+                elif v[4] == '':
+                    v[4] = i[5]
+                elif v[5] == '':
+                    v[5] = i[6]
+                record.update({i[0]: v})
+
+    else:
+        record = {i[0]: i[1:]}
+        new_list_2.append(record)
+pprint(new_list_2)
